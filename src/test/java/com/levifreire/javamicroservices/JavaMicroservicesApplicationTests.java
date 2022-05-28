640b18149481d9ac60f6a4bedf91e26cafc7518c
Levi Freire <levifreire7@gmail.com>
2022-05-28 18:52:12 -0300

    first commit

---
+++ src/test/java/com/levifreire/javamicroservices/JavaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levifreire.javamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
